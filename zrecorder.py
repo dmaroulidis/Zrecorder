12afe9eec027d98b9fe7cf66c7c087b2743426f9
Dimitris Maroulidis <dmaroulidis@dimitrismaroulidis.com>
2021-02-20 22:13:08 +0200

    Add command line arguments

--- zrecorder.py
+++ zrecorder.py
@@ -15,7 +15,8 @@ def main():
     """Main function"""
 
     # Argument parser
-    parser = argparse.ArgumentParser(description='Record Zoom meetings')
+    parser = argparse.ArgumentParser(prog='zrecorder',
+                                     description='Record Zoom meetings')
     parser.add_argument('-m', '--meeting-url', action='store',
                         help='Zoom Meeting URL')
     parser.add_argument('-u', '--username', action='store',
@@ -26,6 +27,10 @@ def main():
                         help='name of course to be recorded')
     parser.add_argument('-o', '-output-dir', action='store', type=Path,
                         help='directory to store recordings')
+    parser.add_argument('-f', '--command-only', action='store_false',
+                        help='join meeting and output only the ffmpeg command')
+
+
 
 
 if __name__ == "__main__":
